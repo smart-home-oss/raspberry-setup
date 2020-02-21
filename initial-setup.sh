f19b2f83de94d2b0dc5cedd57d030763eb49a306
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-02-21 19:21:13 +0100

    added tempf

--- initial-setup.sh
+++ initial-setup.sh
@@ -3,6 +3,8 @@
 # https://www.raspberrypi.org/forums/viewtopic.php?t=34994
 # shellcheck disable=SC2129
 echo "alias temp='/opt/vc/bin/vcgencmd measure_temp'" >> ~/.bash_aliases;
+echo "alias tempf='watch \"/opt/vc/bin/vcgencmd measure_temp\"'" >> ~/.bash_aliases;
+
 echo "alias ll='ls -alF'" >> ~/.bash_aliases;
 echo "alias la='ls -A'" >> ~/.bash_aliases;
 echo "alias l='ls -CF'" >> ~/.bash_aliases;
