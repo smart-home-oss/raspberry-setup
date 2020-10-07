81daf89e253b92dfbf351c9961accdc9ed00c8e5
Rodislav MOLDOVAN <rodislav@gmail.com>
2020-10-07 21:03:52 +0200

    refactored the script, simplified the logic

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,33 +1,40 @@
 #!/usr/bin/env bash
 
-git pull;
-
 # curl -L "https://raw.githubusercontent.com/smart-home-oss/raspberry-setup/master/initial-setup.sh" | bash -
 
-if [ ! -f "$HOME/.bash_aliases" ]; then
-    touch ~/.bash_aliases;
-fi
+mkdir ~/shoss
+git clone https://github.com/smart-home-oss/raspberry-setup.git ~/shoss/raspberry-setup
+
+git pull;
+ALIAS_FILE=~/.bash_aliases_shoss
+
+addAlias() {
+  echo "$1" >> $ALIAS_FILE
+}
+
+rm $ALIAS_FILE
+touch $ALIAS_FILE
 
 # https://www.raspberrypi.org/forums/viewtopic.php?t=34994
-# shellcheck disable=SC2129
-echo "alias temp='/opt/vc/bin/vcgencmd measure_temp'" >> ~/.bash_aliases;
-echo "alias tempf='watch \"/opt/vc/bin/vcgencmd measure_temp\"'" >> ~/.bash_aliases;
+addAlias "alias temp='/opt/vc/bin/vcgencmd measure_temp'"
+addAlias "alias tempf='watch \"/opt/vc/bin/vcgencmd measure_temp\"'"
 
-echo "alias ll='ls -alF'" >> ~/.bash_aliases;
-echo "alias la='ls -A'" >> ~/.bash_aliases;
-echo "alias l='ls -CF'" >> ~/.bash_aliases;
-echo "alias shut='sudo shutdown 0'" >> ~/.bash_aliases;
-echo "alias py='python'" >> ~/.bash_aliases;
-echo "alias py3='python3'" >> ~/.bash_aliases;
+
+addAlias "alias ll='ls -alF'"
+addAlias "alias la='ls -A'"
+addAlias "alias l='ls -CF'"
+addAlias "alias shut='sudo shutdown 0'"
+addAlias "alias py='python'"
+addAlias "alias py3='python3'"
 
 cd ~ || exit 1
-source .bash_aliases
+source .bash_aliases_shoss
 cd - || exit 1
 
+# WiringPi is a PIN based GPIO access library written in C
+# for the BCM2835, BCM2836 and BCM2837 SoC devices
+# used in all Raspberry Pi
 wget https://project-downloads.drogon.net/wiringpi-latest.deb
 sudo dpkg -i wiringpi-latest.deb
-
-mkdir ~/IdeaProjects
-git clone https://github.com/smart-home-oss/raspberry-setup.git ~/IdeaProjects/raspberry-setup
 
 echo "Done, please exit this shell session and open a new one for the changes to become effective."
